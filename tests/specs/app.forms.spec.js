b1d40a88a3b79958955aac303719f8d9427ac63f
Lucas <lucasandrade5990@gmail.com>
2021-04-22 23:54:47 -0300

    first commit

---
+++ tests/specs/app.forms.spec.js
@@ -0,0 +1,11 @@
+
+
+describe('WebdriverIO and Appium, interacting with form elements,', () => {
+    beforeEach(() => {
+      
+    })
+
+    it('should be able type in the input and validate the text', () => {
+        console.log('Android!')
+    })
+})
